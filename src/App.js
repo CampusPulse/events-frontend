cf74c7b29a164314ba259dd5fdf1d00e5323e3e1
Adrian Edwards <adrian@adriancedwards.com>
2024-03-02 15:39:55 -0500

    more content

--- src/App.js
+++ src/App.js
@@ -32,7 +32,12 @@ function App() {
   return (
     <div>
       <h1>Welcome to CampusPulse!</h1>
-      <p class="content">This is a student-built application that aims to combine several sources of events that are happening around you into a single convenient feed, no more flooding of your inbox with newsletters!</p>
+      <p class="content">This is a student-built application that aims to be a proof of concept and minimal viable interface for a project that combines several sources of events that are happening around you into a single convenient feed, no more flooding of your inbox with newsletters!</p>
+
+      <p class="content">Currently this app contains only public data from several sources, including campusgroups, university events, athletics, etc.<br/>If you want to ensure your event makes it to the list, be sure it is part of one of these feeds and is marked as "public".</p>
+
+      <p class="content">This project is <a href="https://github.com/CampusPulse/">open source</a>. Please feel free to contribute if you find new sources of events to include. There are plans to add browsing events by categories, as well as some fun tools for helping you find events if you are ever in need of something new to do.</p>
+
       {events.length === 0 ? (
           <p>Loading...</p>
         ) : (<EventList events={events}></EventList>) }
